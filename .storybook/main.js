fe01f0385b539a4f35efdade922cb76bd9ccb08e
Evgeniy Talko <etalko@gmail.com>
2022-11-04 14:30:14 +0300

    solved with https://github.com/storybookjs/builder-vite/issues/210#issue-1108420294

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,41 @@
+const react = require("@vitejs/plugin-react");
+
 module.exports = {
-  addons: ['@storybook/addon-essentials'],
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: ['../public'],
+  addons: ["@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   features: {
     storyStoreV7: true,
   },
-  framework: '@storybook/react',
+  framework: "@storybook/react",
   core: {
-    builder: '@storybook/builder-vite',
+    builder: "@storybook/builder-vite",
   },
   async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes("vite:react"))
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+      })
+    );
+
+    console.log(config.plugins);
+
     config.optimizeDeps = {
       ...(config.optimizeDeps || {}),
       include: [
         ...(config?.optimizeDeps?.include || []),
-        'msw-storybook-addon',
+        "msw-storybook-addon",
       ],
-    }
-    return config
+    };
+    return config;
   },
-}
+};
